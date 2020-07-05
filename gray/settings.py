0dfdc3428b6de5da61bd3508a335f08c603d826e
Baek-SeungJae <tmdwo897@naver.com>
2020-07-05 21:16:19 +0900

    7/5 버그 픽스

--- gray/settings.py
+++ gray/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 SITE_ID = 2
 SOCIAL_AUTH_URL_NAMESPACE = 'accounts'
 LOGIN_REDIRECT_URL = 'analysis:analysis'
-
+SOCIALACCOUNT_AUTO_SIGNUP = False
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
